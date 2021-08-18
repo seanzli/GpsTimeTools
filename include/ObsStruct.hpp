c6d7c088dcdc1b06f1ba1306ade3b1ec1a63c8d6
seanzli <seanzli@outlook.com>
2021-08-18 20:10:37 +0800

    add comment

--- include/ObsStruct.hpp
+++ include/ObsStruct.hpp
@@ -4,7 +4,7 @@
  * @Author: Sean
  * @Date: 2021-08-18 19:57:00
  * @LastEditors: Sean
- * @LastEditTime: 2021-08-18 20:05:02
+ * @LastEditTime: 2021-08-18 20:10:00
  */
 
 #include <vector>
@@ -16,13 +16,13 @@ namespace Raw{
     using namespace GpsTimeTool;
     class obs {
     public:
-        gtime_t                     time;
-        unsigned char               sat;
-        std::vector<unsigned char>  snr;
-        std::vector<unsigned char>  lli;
-        std::vector<unsigned char>  code;
-        std::vector<double>         L;
-        std::vector<double>         P;
-        std::vector<double>         D;
-    };
+        gtime_t                     time;   // time (gpst)
+        unsigned char               sat;    // satellite number
+        std::vector<unsigned char>  snr;    // signal strength (0.25 dBHz)
+        std::vector<unsigned char>  lli;    // loss of lock indicator
+        std::vector<unsigned char>  code;   // code indicator
+        std::vector<double>         L;      // observation data -> carrier-phase(cycle)
+        std::vector<double>         P;      // observation data -> pseudorange(m)
+        std::vector<double>         D;      // observation data -> doppler frequency(Hz)
+    }; // all vectors should be the same size;
 };
